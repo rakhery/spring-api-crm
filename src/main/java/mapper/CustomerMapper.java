5f39961954ef966b1563cd8b9b3c58a8123a5dad
Christian RAKOTO <crakoto@yahoo.fr>
2022-09-01 09:52:27 +0200

    Tentative de construire Mapper avec les trois méthodes satic

--- src/main/java/mapper/CustomerMapper.java
+++ src/main/java/mapper/CustomerMapper.java
@@ -7,6 +7,18 @@ import org.mapstruct.factory.Mappers;
 
 @Mapper
 public class CustomerMapper {
-    CustomerMapper INSTANCE= Mappers.getMapper(CustomerMapper.class);
-    CustomerDTO CustomerToCustomerDTO(Customer customer);
+    //CustomerMapper INSTANCE= Mappers.getMapper(CustomerMapper.class);
+    //CustomerDTO CustomerToCustomerDTO(Customer customer);
+    public static CustomerDTO buildCustomerDTO(Customer customer){
+        return null;
+    }
+    public static Customer buildCustomer(CustomerDTO customerDTO){
+        return null;
+    }
+    public static void copy(Customer customer,Customer content){
+
+    }
+
+
+
 }

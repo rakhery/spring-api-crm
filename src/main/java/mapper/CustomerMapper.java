639af72851851bcd8e5efccb0182c198a372d389
Christian RAKOTO <crakoto@yahoo.fr>
2022-09-01 10:01:12 +0200

    Interface Mapper

--- src/main/java/mapper/CustomerMapper.java
+++ src/main/java/mapper/CustomerMapper.java
@@ -6,19 +6,11 @@ import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
-public class CustomerMapper {
-    //CustomerMapper INSTANCE= Mappers.getMapper(CustomerMapper.class);
-    //CustomerDTO CustomerToCustomerDTO(Customer customer);
-    public static CustomerDTO buildCustomerDTO(Customer customer){
-        return null;
-    }
-    public static Customer buildCustomer(CustomerDTO customerDTO){
-        return null;
-    }
-    public static void copy(Customer customer,Customer content){
-
-    }
-
-
-
+public interface CustomerMapper {
+    CustomerMapper INSTANCE= Mappers.getMapper(CustomerMapper.class);
+    CustomerDTO CustomerToCustomerDTO(Customer customer);
 }
+
+
+
+
